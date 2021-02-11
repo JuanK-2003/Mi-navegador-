85abefe238b5c6d88de307b81a75d5e553d416bc
Juan Carlos <juancarlosneil@gmail.com>
2021-02-11 12:00:11 -0600

    Se finalizó el proyecto con lo necesario aplicando Regex

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace myBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.textURL = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.newButton = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(3, 12);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(59, 27);
+            this.backButton.TabIndex = 0;
+            this.backButton.Text = "<<";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(68, 12);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(56, 27);
+            this.nextButton.TabIndex = 1;
+            this.nextButton.Text = ">>";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // textURL
+            // 
+            this.textURL.Location = new System.Drawing.Point(130, 17);
+            this.textURL.Name = "textURL";
+            this.textURL.Size = new System.Drawing.Size(607, 22);
+            this.textURL.TabIndex = 2;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchButton.Location = new System.Drawing.Point(743, 12);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 28);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // newButton
+            // 
+            this.newButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.newButton.Location = new System.Drawing.Point(824, 12);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(75, 28);
+            this.newButton.TabIndex = 4;
+            this.newButton.Text = "New Tab";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Location = new System.Drawing.Point(3, 45);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(896, 509);
+            this.tabControl.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webBrowser);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(888, 480);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(882, 474);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 554);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.textURL);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.backButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.TextBox textURL;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myBrowser
+{
+    public partial class Form1 : Form
+    {
+        WebBrowser webTab = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser.Navigate("https://www.google.com/");
+            webBrowser.DocumentCompleted += WebBrowser_DocumentCompleted;
+        }
+
+        private void WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tabControl.SelectedTab.Text = webBrowser.DocumentTitle;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            WebBrowser web = tabControl.SelectedTab.Controls[0] as WebBrowser;
+            if( web != null)
+            {
+                Regex isSearch = new Regex("^¿?([\\wáéíóú]+\\s?)+\\??$");
+                if( isSearch.IsMatch(textURL.Text))
+                {
+                    web.Navigate($"http://www.google.com/search?q={textURL.Text.Trim()}");
+                    textURL.Text = $"http://www.google.com/search?q={textURL.Text.Trim()}";
+                    web.DocumentCompleted += WebBrowser_DocumentCompleted;
+                }
+                else
+                {
+                    textURL.Text = normURL(textURL.Text);
+                    web.Navigate(textURL.Text);
+                }
+            }
+        }
+
+        private string normURL(string text)
+        {
+            text.Trim();
+            //^(https)?(http)?(://)?\w{3}?\.?\w+\.?\w{3,5}.*$
+            Regex completeURL = new Regex("^(https)?(http)?(://)?\\w{3}?\\.?\\w+\\.?\\w{3,5}.*$");
+            if( !completeURL.IsMatch(text))
+            {
+                return $"https://{text}";
+            }
+            return text;
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            TabPage tab = new TabPage();
+            tab.Text = "New Tab";
+            tabControl.Controls.Add(tab);
+            tabControl.SelectTab(tabControl.TabCount - 1);
+            webTab = new WebBrowser() { ScriptErrorsSuppressed = true };
+            webTab.Parent = tab;
+            webTab.Dock = DockStyle.Fill;
+            webTab.Navigate("https://www.google.com/");
+            webTab.DocumentCompleted += WebBrowser_DocumentCompleted;
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            WebBrowser web = tabControl.SelectedTab.Controls[0] as WebBrowser;
+            if( web != null )
+            {
+                if( web.CanGoBack )
+                {
+                    web.GoBack();
+                }
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            WebBrowser web = tabControl.SelectedTab.Controls[0] as WebBrowser;
+            if (web != null)
+            {
+                if (web.CanGoForward)
+                {
+                    web.GoForward();
+                }
+            }
+        }
+    }
+}
